2870a366519b2aa124d565af26b37a8ef6f6567b
Joe Edelman <joe.edelman@gmail.com>
2010-09-05 18:30:26 -0700

    new login logic

--- js/base.js
+++ js/base.js
@@ -10,21 +10,97 @@ function watch_location(){
 
 
 go.push({
+  facebook_ready: function() {
+    This.facebook_ready = true;
+    // TODO: check for twitter below too
+    if (This.facebook_ready) go.trigger('social_logins_ready');
+  },
+  
+  social_logins_ready: function() {
+    // get tokens from server if necessary
+    if (This.facebook_uid && !window.authority) return go('#fetch_login_token_from_facebook');
+    
+    // otherwise user ready
+    go.trigger('will_user_ready');
+    go.dispatch('user_ready') || go('#user_ready_default');
+  },
+  
+  will_user_ready: function() {
+    if (window.authority) $('body').addClass( 'logged_in' );
+    else $('body').addClass( 'logged_out' );
+  },
+  
+  user_ready_default: function() {
+    if (window.authority) go.dispatch('user_ready_logged_in') || go('#user_ready_logged_in_default');
+    else go.dispatch('user_ready_logged_out') || go('#redirect("/login")');
+  },
+  
+  user_ready_logged_in_default: function() {
+    watch_location();
+    go('tool=start');
+  },
+  
+  
+  facebook_login: go.f('#fetch_login_token_from_facebook'),
+  
+  fetch_login_token_from_facebook: function() {
+    $.post('/api/me/contact_methods', {'url': 'facebook:' + This.facebook_uid}, function(){
+      var user = $.cookie('gcuser');
+      if (user) $.extend(window, eval('(' + user + ')'));
+      if (!This.login_after_page_load) window.location.reload();
+      else go.dispatch('login') || window.location.reload();
+    });
+  },
+  
   report_error: function() {
     $.post('/api/bugreport', {issue: This.bugreport}, go.f('#notify_error'));
   },
   
-  facebook_login: function() {
-    $.post('/api/me/contact_methods', {'url': 'facebook:' + This.facebook_uid}, function(){
-      var user = $.cookie('gcuser');
-      if (user) $.extend(window, eval('(' + user + ')'));
-      go.dispatch('facebook_did_login') || window.location.reload();
-    });
+  facebook_logout: function(){ 
+    go('#redirect("/api/logout")');
   },
   
-  facebook_logout: function(){ 
-    window.location.href = '/api/logout';
-  }
+  redirect: function(url) {
+    window.location.href = url;
+  },
+  
+  
+  // signup specific
+  
+  mobile_form_submitted: function(){
+    $.post(
+      '/api/me/verifications', {
+        'url': 'tel:' + This.form_data.mobile,
+        'stream': current_stream
+      }, go.f('#self_added')
+    );
+  },
+  
+  add_self: function() {
+    $.post('/api/'+current_stream+'/members', go.f('#added_self_to_stream'));
+  },
+  
+  added_self_to_stream: function() {
+    if (window.user_mobile) go('#self_added_need_mobile');
+    else go('#self_added');
+  },
+  
+  self_added_mobile_needed: go.f('tool=get_mobile'),
+  
+  pw_form_submitted: function() {
+    $.post('/api/me', {'password': This.form_data.pw}, go.f('tool=iphone'));
+  },
+
+  
+  // roles & auth
+  stream_role_organizer: function() {
+    return window.stream_role == 'leader' || window.stream_role == 'organizer';
+  },
+
+  // data //
+  viewer_url: function() { return 'http://groundcrew.us/'+current_stream; },
+  invite_url: function() { return 'http://groundcrew.us/'+current_stream+'/signup'; },
+  join_url: function() { return 'http://groundcrew.us/'+current_stream+'/join'; }
 });
 
 This.user = { tag: 'pAnon'};
