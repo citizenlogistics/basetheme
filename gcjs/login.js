ce9023f81b2ae19598b47e6ec5983aa93e9ab54d
Noah Miller <noahhmiller@gmail.com>
2010-10-14 13:04:36 -0400

    comments

--- gcjs/login.js
+++ gcjs/login.js
@@ -1,6 +1,5 @@
 go.push({
   login_form_submitted: function(data) {
-    // TODO: handle error
     return $.post_with_squad('/login', data, function() {
       go('#complete_auth_from_cookie');
       go.dispatch('login_complete') || go('#login_complete_default');
--- gcjs/signup.js
+++ gcjs/signup.js
@@ -1,6 +1,5 @@
 Signup = {
   signup_form_submitted: function(data) {
-    // TODO: handle error
     return $.post_with_squad('/people/join', data, function() {
       go('#complete_auth_from_cookie');
       go('tool=signup_details');
@@ -13,14 +12,13 @@ Signup = {
     return window.current_stream_systems && window.current_stream_systems.contains('m');
   },
 
-  // TODO: go needs to handle multipart/form-data
+  // TODO: support image uploading
   signup_details_form_submitted: function(data) {
     if (!data.m_sysid && Signup.request_user_mobile()) {
        var cont = confirm('Groundcrew works better with text messaging using your cell phone.\n\nClick Cancel to enter a mobile number, or OK to continue without one.');
        if (!cont) return "redo";
     }
 
-    // TODO: handle error
     return $.post_with_squad('/agents/update', data, function() {
       go('#complete_auth_from_cookie');
       go.dispatch('signup_complete') || go('#signup_complete_default');
