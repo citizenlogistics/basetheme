3c021ab45863c70f11a521befbaaee1b723f1058
Noah Miller <noahhmiller@gmail.com>
2010-10-20 11:09:28 -0400

    StreamLoader#load errors if atevent is null

--- gcjs/stream_loader.js
+++ gcjs/stream_loader.js
@@ -14,39 +14,36 @@ StreamLoader = {
     return This.user.tag + '_' + new Date().getTime();
   },
 
-  init: function(stream_url) {
-    StreamLoader.stream_url = stream_url;
-    $("body").bind("ajaxSend", function(){
-      clearTimeout(StreamLoader.timer);
-    }).bind("ajaxComplete", function(event, req, settings){
-      if (!req || req.status != 403 || !settings || !settings.url || settings.url.indexOf(StreamLoader.stream_url) < 0) {
-        StreamLoader.schedule_autoload();
-      }
-    });
-
-    StreamLoader.autoload();
-  },
-
-  schedule_autoload: function(interval){
-    if (StreamLoader.timer) clearTimeout(StreamLoader.timer);
-    StreamLoader.timer = setTimeout(function(){StreamLoader.autoload();}, interval || StreamLoader.interval);
-  },
-
-  autoload: function(callback){
+  load: function(success, error){
     var uri = StreamLoader.stream_url;
     if (atevent) uri += '&since=' + atevent;
     if (This.city_id) {
       uri += '&city=' + This.city_id;
     }
 
-    $.getScript(uri, callback);
+    $.ajax({ url: uri, dataType: 'script', error: error, success: function() {
+      if (!atevent) error && error(); // if atevent is null, then stream script eval did not complete
+      else success && success();
+    }});
   },
 
-  fetch: function(url, options, after){
-    $.getJSON(url, options, function(obj){
-      if (obj.error){ alert("Note: " + obj.error); return; }
-      if (after) after(obj);
+  init_autoload: function() {
+    $("body").bind("ajaxSend", function(){
+      // reduce network congestion by delaying stream load on any other ajax call
+      clearTimeout(StreamLoader.timer);
+    }).bind("ajaxComplete", function(event, req, settings){
+      // schedule stream load whenever an ajax call completes, unless its a stream load that
+      // resulted in a permissions error
+      if (req && req.status == 403 && settings && settings.url && settings.url.contains(StreamLoader.stream_url)) return;
+      StreamLoader.schedule_autoload();
     });
+
+    StreamLoader.load();
+  },
+
+  schedule_autoload: function(interval){
+    if (StreamLoader.timer) clearTimeout(StreamLoader.timer);
+    StreamLoader.timer = setTimeout(function(){StreamLoader.load();}, interval || StreamLoader.interval);
   }
 
 };
