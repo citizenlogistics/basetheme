a6ccac196bca2b98a4f04aef3cfbbffbf71e2d6a
Matt Chisholm <matt@theory.org>
2010-11-22 12:04:32 -0800

    cleaning up did_login

--- js/base.js
+++ js/base.js
@@ -122,12 +122,8 @@ go.push({
     $.post('/api/me/contact_methods', {'url': 'facebook:' + This.facebook_uid}, function(){
       var user = $.cookie('gcuser');
       if (user) $.extend(window, eval('(' + user + ')'));
-      if (!This.login_after_page_load) window.location.reload();
-      else {
-        if (This.tool == 'login') go('tool=');
-        go('#complete_auth_from_cookie');
-        go.dispatch('did_login') || window.location.reload();
-      } 
+      if (This.tool == 'login') go('tool=');
+      go.dispatch('did_login') || window.location.reload();
     });
   },
 
